b1c7de40e7e26f66d4ada08061fbc4d57fe71ee4
Oscar David García López <ogarcial9@miumg.edu.gt>
2026-08-08 11:42:39 -0600

    Pagina de inicio (home)

---
+++ proyecto/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/pagina1')
+def pagina1():
+    return render_template('pagina1.html')
+
+@app.route('/pagina2')
+def pagina2():
+    return render_template('pagina2.html')
+
+@app.route('/pagina3')
+def pagina3():
+    return render_template('pagina3.html')
+
+@app.route('/pagina4')
+def pagina4():
+    return render_template('pagina4.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
