c94d5a8f76ea2b5da607344fb9123fca3a32c264
Oscar David García López <ogarcial9@miumg.edu.gt>
2026-08-08 12:26:24 -0600

    Agregar funciones Flask y formulario

---
+++ proyecto/README.md
@@ -0,0 +1,23 @@
+# Proyecto Desarrollo Web
+
+Este es el proyecto realizado para el primer parcial de Desarrollo Web.
+
+Se trabajo con HTML y CSS para hacer las paginas y con Flask para poder navegar entre ellas.
+
+El proyecto tiene una pagina de inicio y 4 paginas mas donde se utilizaron tablas, formularios, listas, imagenes y etiquetas semanticas.
+
+Tambien se utilizo Flask para las rutas, formularios con GET y POST y variables con Jinja2.
+
+## Para ejecutar
+
+Abrir la terminal en la carpeta del proyecto y ejecutar:
+
+python app.py
+
+Luego abrir la direccion que muestra Flask en el navegador.
+
+## Debug
+
+Se utilizo debug=True para poder hacer las pruebas durante el desarrollo y ver los errores mas facilmente.
+
+debug=True se utiliza solamente durante el desarrollo y no se debe utilizar cuando la aplicacion esta en produccion.
--- proyecto/app.py
+++ proyecto/app.py
@@ -1,30 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def inicio():
     return render_template('index.html')
 
+
 @app.route('/pagina1')
 def pagina1():
     return render_template('pagina1.html')
 
-@app.route('/pagina2')
+
+@app.route('/pagina2', methods=['GET', 'POST'])
 def pagina2():
+
+    if request.method == 'POST':
+
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+
+        mensaje = 'Registro realizado correctamente'
+
+        return render_template(
+            'pagina2.html',
+            mensaje=mensaje,
+            nombre=nombre,
+            correo=correo
+        )
+
     return render_template('pagina2.html')
+
 
 @app.route('/pagina3')
 def pagina3():
     return render_template('pagina3.html')
 
+
 @app.route('/pagina4')
 def pagina4():
     return render_template('pagina4.html', nombre='Estudiante')
 
+
 @app.route('/estudiante/<nombre>')
 def estudiante(nombre):
     return render_template('pagina4.html', nombre=nombre)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
